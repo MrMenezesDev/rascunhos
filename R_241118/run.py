116dffb32468ed88328129f443ddae2dd2bb91c2
Erick Menezes <erick.menezes@cubos.io>
2024-11-18 16:34:03 -0300

    feat: adicionado gerador de gif

--- R_241118/run.py
+++ R_241118/run.py
@@ -1,27 +1,34 @@
 import py5
+from utils.image_utils import save_frames
 
 from hex.hex_grid import HexGrid
+from utils.image_utils import create_gif
 from utils.p5_utils import draw_map, draw_vertex
 from hex.hex_wave_collapse import HexWaveFunctionCollapseGrid
+import random
+import sys
 
 hex_grid = None
-
+if len(sys.argv) > 1:
+    seed = int(sys.argv[1])
+else:
+    seed = random.randint(0, 10000)
 
 def settings():
-    py5.size(512 + 256, 512)  # Defina o tamanho da tela
-
+    size = 768
+    py5.size(int(size * 1.5) , size)  # Defina o tamanho da tela
 
 def setup():
     global hex_grid
-    col = 10
-    row = 5
+    random.seed(seed)
+    col = 15
+    row = 8
     grid = HexGrid(cols=col, rows=row, size=50, draw_vertex=draw_vertex, draw_map=draw_map)
     grid.draw()
     hex_grid = HexWaveFunctionCollapseGrid(
         dim=[col, row], draw_cell=grid.draw_cell, border=True
     )
     hex_grid.start()
-
 
 def draw():
     global hex_grid
@@ -30,5 +37,11 @@ def draw():
     if hex_grid.complete:
         py5.no_loop()
 
-py5.run_sketch()
+def key_pressed():
+    global seed, frames_dir
+    if py5.key == 's' or py5.key == 'S':
+        create_gif(frames_dir, seed)
 
+
+frames_dir = save_frames(seed, limit=None, start=0)
+py5.run_sketch(block=False)
---
+++ utils/image_utils.py
@@ -0,0 +1,31 @@
+import os
+import imageio
+import py5_tools
+
+
+def save_frames(seed, limit=50, start=0):
+    frames_dir = f"./tmp/frames_{seed}/"
+    py5_tools.save_frames(frames_dir, limit=limit, start=start)
+    return frames_dir
+
+
+def create_gif(frames_dir, seed):
+    # Lista todos os arquivos na pasta e filtra apenas os arquivos PNG
+    frame_files = sorted([f for f in os.listdir(frames_dir) if f.endswith(".png")])
+    print(f"Creating GIF with {len(frame_files)} frames.")
+    print(f"Frames dir: {frames_dir}")
+    print(f"Seed: {seed}")  
+    # Lê as imagens e as adiciona à lista
+    images = []
+    for frame_file in frame_files:
+        filename = os.path.join(frames_dir, frame_file)
+        images.append(imageio.imread(filename))
+
+    # Verifica se há imagens antes de criar o GIF
+    if images:
+        # Cria o GIF com o nome da pasta incluído
+        gif_filename = f"./tmp/animation_{seed}.gif"
+        imageio.mimsave(gif_filename, images, duration=0.1)
+    else:
+        print("No images found to create GIF.")
+    print(f"Animation saved as {gif_filename}")
